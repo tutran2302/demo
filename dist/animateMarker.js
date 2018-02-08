55d5c3bad6decfdf345e000024090f474d8df83e
tutran2302 <36067003+tutran2302@users.noreply.github.com>
2018-02-08 14:19:08 +0700

    Update animateMarker.js

--- dist/animateMarker.js
+++ dist/animateMarker.js
@@ -1,36 +1,48 @@
-var state=["run","stop"],speed=100,markerstate=state[0],myreg;
-function run(length){
-    marker = L.marker(markersList[length-1].getLatLng(), {icon: carIcon}).setRotationOrigin('center center').bindPopup(markersList[0]._popup._content).openPopup().addTo(map);
-    markEnd = L.marker(markersList[0].getLatLng(), {icon: carIcon}).setRotationOrigin('center center').bindPopup(markersList[0]._popup._content).addTo(map);
-    var j = length-2;
-    markerstate=state[0];
+//leaflet-rotatedmarker.js"
+var animateIcon = L.icon({
+    iconUrl: 'icon/car_1.png',
+    iconSize:[50,39],
+    iconAchor:[20,20],
+    popupAnchor:[0,0]
+});
+var state=["run","stop"],animateState=state[0],myreg;
+function run(markersList,speed){
+    marker = L.marker(markersList[0].getLatLng(), {icon: animateIcon}).setRotationOrigin('center center').bindPopup(markersList[0]._popup._content).addTo(map).openPopup();
+    markEnd = L.marker(markersList[markersList.length-1].getLatLng(), {icon: animateIcon}).setRotationOrigin('center center').bindPopup(markersList[0]._popup._content).addTo(map);
+    var length = markersList.length-1;
+    var j=0;
+    animateState=state[0];
     myreg =window.requestAnimationFrame(jump);
     function jump() {
-        if (j <0||markerstate==="stop") {
-            marker.openPopup();
-            window.cancelAnimationFrame(myreg);
-        }
-        else {
-            marker.setLatLng(markersList[j].getLatLng()).setPopupContent(markersList[j]._popup._content).setRotationAngle(markersList[j].properties.carHeading).update();
+        if (j===length) {
+            animateState=state[1];
+            map.removeLayer(marker);
+            markEnd.openPopup();
+        } else {
+            if(animateState==="stop") {
+                marker.openPopup();
+                window.cancelAnimationFrame(myreg);
+            } else{
+            marker.setLatLng(markersList[j].getLatLng()).setPopupContent(markersList[j]._popup._content).setRotationAngle(markersList[j].properties.carHeading).openPopup();
             var mapbound = map.getBounds();
             var inbound = mapbound.contains(marker.getLatLng());
             if (!inbound) map.setView(marker.getLatLng());
             window.cancelAnimationFrame(myreg);
             if (markersList[j].properties.type==="stop") {
                 setTimeout(function () {
-                    j--;
+                    j++;
                     myreg=window.requestAnimationFrame(jump);
                 },1000)}
                 else {
                     setTimeout(function () {
-                        j--;
+                        j++;
                         myreg=window.requestAnimationFrame(jump);
                     },1000/speed)
-                }}}
+                }}}}
     marker.on('click', function () {
-        if (markerstate==="stop") {
-            markerstate = state[0];
+        if (animateState===state[1]) {
+            animateState = state[0];
             myreg=window.requestAnimationFrame(jump);
         }
-        else markerstate=state[1];
+        else animateState=state[1];
     });}
